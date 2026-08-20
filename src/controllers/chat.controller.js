755c9314eb5236b297e47cb8197778e593233fea
Hariom <hariom@rizilianttech.com>
2026-08-20 13:07:18 +0530

    rc

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -57,6 +57,7 @@ const getChats = async (req, res, next) => {
         : false;
       return {
         id:           c.id,
+        matchId:     chat.matchId,
         buddyId:      buddy.id,
         buddyName:    `${buddy.firstName} ${buddy.lastName}`,
         buddyAvatar:  buddy.avatarUrl,
